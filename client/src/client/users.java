5ff70f30f4b67f7d2f9674ea3e610f3cb0fcb254
cloudspurs <443051430@qq.com>
2016-03-28 18:24:58 +0800

    03-28

---
+++ client/src/client/users.java
@@ -0,0 +1,15 @@
+package client;
+
+public class users {
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
